c2be8fb7b5e446dc8809afbd885948722c2c0ef4
Andrew Mattie <andrewmattie@mattieapps.com>
2017-12-17 00:48:30 -0500

    Basic UI setup

--- app/src/main/java/com/mattieapps/clearnote/MainActivity.kt
+++ app/src/main/java/com/mattieapps/clearnote/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.mattieapps.clearnote
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        notesListView.set
     }
 }
